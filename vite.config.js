a836e98c74fae1d9ce2cbd28a01b58e23954010d
Nameera046 <2315030@nec.edu.in>
2025-11-30 16:51:17 +0530

    base URL Updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",    // MUST BE ROOT FOR VERCEL
+  base:process.env.BASE_APP || "/Alumni-Webinar-Project",
 });
